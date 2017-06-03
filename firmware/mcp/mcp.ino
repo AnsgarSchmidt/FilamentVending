b7c7450c70641918a8b8f60a0b540aa72a94372e
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-06-03 14:05:39 +0200

    Adding basic firmware file

---
+++ firmware/mcp/mcp.ino
@@ -0,0 +1,12 @@
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
